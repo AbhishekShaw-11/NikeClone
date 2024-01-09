6463f44b581e07fa99588f09298ca5e257e28405
abhishek 1199 <abhishekshaw1199@gmail.com>
2024-01-09 14:13:54 +0530

    4

--- e-commerce/src/Component/Home.jsx
+++ e-commerce/src/Component/Home.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
-import Nav from './Nav'
+import React from "react";
+import Nav from "./Nav";
+import Nikebanner from "./Nikebanner.png";
 const Home = () => {
   return (
-  <React.Fragment>
-      <Nav/>
-      <div className='  md:text-center bg-gray-100 text-black'>
-      <h4 className=''>Move,Shop,Customise & Celebrate With Us.
-      </h4>
-      <p className=''>
-      No matter what you feel like doing today, It's better as a member .
-      </p>
-      <a className='underline'><span>Join Us.</span></a>
+    <React.Fragment>
+      <Nav />
+      <div className="  md:text-center bg-gray-100 text-black">
+        <h4 className="">Move,Shop,Customise & Celebrate With Us.</h4>
+        <p className="">
+          No matter what you feel like doing today, It's better as a member .
+        </p>
+        <a className="underline">
+          <span>Join Us.</span>
+        </a>
       </div>
-    </React.Fragment>
-  )
-}
+    
 
-export default Home
+      <div className="Banner mx-20 border-black-800 ">
+        <img src={Nikebanner}></img>
+      </div>
+ <div className = "">
+ 
+ </div>
+
+    </React.Fragment>
+  );
+};
+
+export default Home;
